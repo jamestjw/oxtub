9658454ef3b49e06e73873e6d75f2e23b49ffe94
James Tan <james_tan97@outlook.com>
2026-05-24 00:24:11 -0400

    ensure pages are aligned

--- src/buffer/page.rs
+++ src/buffer/page.rs
@@ -1,4 +1,31 @@
+use crate::storage::disk::config::DEFAULT_PAGE_SIZE;
+
+#[repr(align(8))]
+pub struct PageData<const N: usize>(pub [u8; N]);
+
 pub struct Page {
     page_id: Option<usize>,
-    data: Vec<u8>,
+    data: PageData<DEFAULT_PAGE_SIZE>,
+}
+
+impl Page {
+    pub fn new() -> Self {
+        Self {
+            page_id: None,
+            data: PageData([0; DEFAULT_PAGE_SIZE]),
+        }
+    }
+
+    pub fn data(&self) -> &[u8] {
+        &self.data.0
+    }
+    pub fn data_mut(&mut self) -> &mut [u8] {
+        &mut self.data.0
+    }
+    pub fn page_id(&self) -> Option<usize> {
+        self.page_id
+    }
+    pub fn set_page_id(&mut self, page_id: Option<usize>) {
+        self.page_id = page_id;
+    }
 }
