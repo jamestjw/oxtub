ca5816baff8d20f16620be2e53e77763fef8cc90
James Tan <james_tan97@outlook.com>
2026-07-01 20:03:43 -0400

    implement projection executor

--- src/query/executor/projection.rs
+++ src/query/executor/projection.rs
@@ -1,7 +1,10 @@
 use crate::{
     catalog::schema::Schema,
     query::{
-        executor::{engine::ExecutorRow, error::ExecutionError, executor::Executor},
+        executor::{
+            engine::ExecutorRow, error::ExecutionError, executor::Executor,
+            expression::evaluate_expression,
+        },
         planner::plan::ProjectionPlan,
     },
 };
@@ -33,7 +36,23 @@ impl Executor for ProjectionExecutor<'_> {
 
     fn next(&mut self, batch_size: usize) -> Result<Vec<ExecutorRow>, ExecutionError> {
         let batch = self.child.next(batch_size)?;
-        todo!("next projection executor")
+        let mut res = Vec::with_capacity(batch.len());
+
+        for row in batch {
+            let row_values = self
+                .plan
+                .expressions
+                .iter()
+                .map(|expr| evaluate_expression(expr, &row))
+                .collect::<Result<Vec<_>, _>>()?;
+
+            res.push(ExecutorRow {
+                rid: None,
+                values: row_values,
+            });
+        }
+
+        Ok(res)
     }
 
     fn output_schema(&self) -> &Schema {
